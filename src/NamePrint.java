8cffa350279332c3fa888567c16fe9dc5d0bdd65
Jakub <jakubkrys@gmail.com>
2020-12-27 19:40:17 +0100

    #3 Program prezentujący imię

---
+++ src/NamePrint.java
@@ -0,0 +1,12 @@
+public class NamePrint {
+    public static void main(String[] args) {
+
+        char[] name = {'M','a','r','e','k'};
+        int index = 0;
+
+        while(index < name.length){
+            System.out.print(name[index]);
+            index++;
+        }
+    }
+}
